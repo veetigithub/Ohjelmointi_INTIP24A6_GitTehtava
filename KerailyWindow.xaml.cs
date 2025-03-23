cecd7eeea1f7fac44bf69664c5d7f4bef04c1fcc
Veeti Vesterinen <veeti12.vesterinen@gmail.com>
2025-03-24 00:11:01 +0200

    prodDatan muutoksia, inventaarioWindowiin tulee prodData esille

--- KerailyWindow.xaml.cs
+++ KerailyWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
